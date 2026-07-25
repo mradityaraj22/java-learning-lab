efcde00b4db8bbae612469c9bfa1997c6ed8e07f
intervier22 <mradityaraj22@gmail.com>
2026-07-25 16:59:54 +0530

    literal

---
+++ literals/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    IO.println(String.format("Hello and welcome!"));
+    int nnum1=2;
+    float hii=23.4f;
+    long longitn=423232424l;
+    char hell = 'a';
+    String name = "aer";
+    IO.println(name);
+
+}
