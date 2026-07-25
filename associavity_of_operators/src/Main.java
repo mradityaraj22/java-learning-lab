1815521f6a484a62a968031dc8b44f7df1b4bfe3
intervier22 <mradityaraj22@gmail.com>
2026-07-25 18:05:36 +0530

    associativtiy and precedence

--- associavity_of_operators/src/Main.java
+++ associavity_of_operators/src/Main.java
@@ -1,5 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    int a = 6*5-34/2;
+    System.out.println(a);
+//HIGEHST PRECEDENCE GOES TO STAR AND SLASH THEY ARE GIVEN LEFT TO RIGHT ASSOCIATIVITY
+//    LEFT TO RIGHT CHALEGA *,/,% ME AGAR EK SATH HO TABHIIIIII
+    int b = 60/5 - 34*2;
+    System.out.println(b);
+//    PRECEDENCE AND ASSOCOIAITVIYT
 
+//    QUICK QUIZ
+    int x = 6;
+    int y =1;
+    int k = x * y/2;
+    System.out.println(k);
+    int quad = (x*x - (4*x*y))/2*y;
+    System.out.println(quad);
 }
