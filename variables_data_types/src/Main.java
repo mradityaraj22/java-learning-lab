a96a6bab8ebd4fe69109df8bc51190d9ee7a3af2
intervier22 <mradityaraj22@gmail.com>
2026-07-25 16:39:29 +0530

    variable and data type

---
+++ variables_data_types/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    IO.println(String.format("Hello and welcome!"));
+
+    int whatDo = 7;
+    IO.println(whatDo);
+    int num1= 7;
+    int num2 = 4;
+    int sum = num1+num2;
+    IO.println(sum);
+
+}
