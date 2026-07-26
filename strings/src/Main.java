933e9d7cc95b5c579d2be1eeed511016ccb5d400
intervier22 <mradityaraj22@gmail.com>
2026-07-26 15:03:15 +0530

    string basic

---
+++ strings/src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+      char grade = '8';
+//      encrypting
+      grade = (char)(grade +8);
+    System.out.println(grade);
+//    decrypting
+    grade = (char)(grade - 8);
+    System.out.println(grade);
+
+//    String str = new String("harry");
+    String str = "harry";
+    System.out.println(str);
+    int a =6;
+    System.out.printf("The value of a is %d", a);
+    Scanner sc = new Scanner(System.in);
+    String st = sc.nextLine();
+    System.out.println(st);
+
+
+}
